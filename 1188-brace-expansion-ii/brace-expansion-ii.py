2700461b9483032a6d99826c4fe8185ae289e7f7
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:34:00 +0530

    Update brace-expansion-ii.py

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -10,10 +10,7 @@ class Solution:
                 stack.append(union)
                 stack.append(prod)
                 union, prod = [], [""]
-            elif c == '}':
-                pre_prod, pre_union = stack.pop(), stack.pop()
-                prod = [p + v for p, v in product(pre_prod, union + prod)]
-                union = pre_union
+         
          
 
 
