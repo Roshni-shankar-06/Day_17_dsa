d6da16a42c25bd82db29aac5168492a0353d0e74
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:34:23 +0530

    Remove unused imports and class definition

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -1,10 +1,6 @@
-from itertools import product
 
-class Solution:
-    def braceExpansionII(self, expression: str) -> list[str]:
-        stack, union, prod = [], [], [""]
-        for c in expression:
-            if c.isalpha():
+
+
        
          
 
