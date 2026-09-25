ece1b9f29983874200166f576a097ab5d3bf9d11
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:34:10 +0530

    Refactor braceExpansionII method
    Refactor braceExpansionII method to improve readability.

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -5,12 +5,7 @@ class Solution:
         stack, union, prod = [], [], [""]
         for c in expression:
             if c.isalpha():
-                prod = [v + c for v in prod]
-            elif c == '{':
-                stack.append(union)
-                stack.append(prod)
-                union, prod = [], [""]
-         
+       
          
 
 
