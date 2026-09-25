688c318491ed5f8aedb1697a0bb477a60aaa1194
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:27:53 +0530

    Update min-stack.java

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,10 +1,6 @@
 import java.util.Stack;
 
-class MinStack {
-    // A private helper class to store the value and the minimum up to this point
-    private static class Node {
-        int val;
-        int min;
+
 
     
      
