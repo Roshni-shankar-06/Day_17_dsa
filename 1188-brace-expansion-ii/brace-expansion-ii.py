f2932db73630ecdc8235ff0aac96eabd4d0b18ed
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:34:32 +0530

    Add braceExpansionII method for string expansion
    Implement brace expansion using a stack to handle nested expressions.

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -1,4 +1,23 @@
+from itertools import product
 
+class Solution:
+    def braceExpansionII(self, expression: str) -> list[str]:
+        stack, union, prod = [], [], [""]
+        for c in expression:
+            if c.isalpha():
+                prod = [v + c for v in prod]
+            elif c == '{':
+                stack.append(union)
+                stack.append(prod)
+                union, prod = [], [""]
+            elif c == '}':
+                pre_prod, pre_union = stack.pop(), stack.pop()
+                prod = [p + v for p, v in product(pre_prod, union + prod)]
+                union = pre_union
+            elif c == ',':
+                union += prod
+                prod = [""]
+        return sorted(set(union + prod))
 
 
        
