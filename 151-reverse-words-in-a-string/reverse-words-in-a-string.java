ecfc068c80550a143b300d5dfda1cebe229c9ae3
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:30:49 +0530

    Update reverse-words-in-a-string.java

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,8 +1,3 @@
 class Solution {
     public String reverseWords(String s) {
-        // Trim leading/trailing spaces and split by any sequence of spaces
-        String[] words = s.trim().split("\\s+");
-        StringBuilder result = new StringBuilder();
-        
-        // Iterate backwards through the array of words
-      
+     
