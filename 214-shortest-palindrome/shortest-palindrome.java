0d5ff0cb3dd2c7cb522896e6ec3372788a55d9e0
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:24:38 +0530

    Refactor palindrome logic by simplifying string handling
    Removed unnecessary concatenation and LPS array initialization.

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -5,8 +5,4 @@ class Solution {
         }
         
         String rev = new StringBuilder(s).reverse().toString();
-        String temp = s + "#" + rev;
-        int[] lps = new int[temp.length()];
-        
-        for (int i = 1; i < temp.length(); i++) {
-           
+       
