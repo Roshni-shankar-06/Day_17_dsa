211a7e2ebe894194b0e5fca47ddc9d9d4cc40c5c
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:29:24 +0530

    Refactor loop structure in maximum-product-subarray

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -10,11 +10,4 @@ class Solution {
         int ans = nums[0];
 
         // Iterate through the array starting from the second element
-        for (int i = 1; i < nums.length; i++) {
-            int current = nums[i];
-            
-            // Store maxProd before updating because minProd needs the old value
-            int tempMax = maxProd;
-
-            // Calculate the 3 possible choices for the new max and min
-            
+        fo
