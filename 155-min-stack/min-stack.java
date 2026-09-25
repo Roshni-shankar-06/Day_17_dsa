8ecf49240a22d660ec6ea51054fff56736a59c7d
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:25:48 +0530

    Remove getMin method from MinStack
    Removed getMin method and related comments from MinStack.

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -44,16 +44,4 @@ class MinStack {
     }
     
     /** Retrieve the minimum element in the stack. */
-    public int getMin() {
-        return stack.peek().min;
-    }
-}
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+ 
