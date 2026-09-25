d42f7a7e02e49cda97c25a9a9869b0c226c4f320
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:24:48 +0530

    Update shortest-palindrome.java

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,8 +1,3 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        if (s == null || s.length() == 0) {
-            return s;
-        }
-        
-        String rev = new StringBuilder(s).reverse().toString();
-       
+        i
