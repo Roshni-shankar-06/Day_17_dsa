40c5136d0764f49d94954c112d70c0e6c7ee676a
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:31:31 +0530

    Refactor day calculation logic in day-of-the-year.py
    Removed the calculation of days in months and the summation of previous months' days.

--- 1260-day-of-the-year/day-of-the-year.py
+++ 1260-day-of-the-year/day-of-the-year.py
@@ -8,7 +8,4 @@ class Solution:
         is_leap = (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0)
         feb_days = 29 if is_leap else 28
         
-        days_in_months = [31, feb_days, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        
-        # Sum days of previous months plus current day
-        return sum(days_in_months[:month - 1]) + day
+   
