23a746b4548a97375b29cd2f61ed6b3d13d20282
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:31:41 +0530

    Remove leap year check from day-of-the-year.py
    Removed leap year check and related logic.

--- 1260-day-of-the-year/day-of-the-year.py
+++ 1260-day-of-the-year/day-of-the-year.py
@@ -4,8 +4,5 @@ class Solution:
         month = int(date[5:7])
         day = int(date[8:])
         
-        # Check for leap year
-        is_leap = (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0)
-        feb_days = 29 if is_leap else 28
-        
+    
    
