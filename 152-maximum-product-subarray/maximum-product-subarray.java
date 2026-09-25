ef71fecdb6815d2093dd35befcdec28d9ab7f3d2
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:29:46 +0530

    Refactor maxProduct method input validation
    Remove null and empty check for input array.

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,8 +1,3 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        // Initialize tracking variables with the first element
-        int maxProd = nums[0];
+    
