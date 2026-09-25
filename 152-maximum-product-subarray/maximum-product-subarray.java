0a900705d9f4c9686e533aeba827c4c6b5fbe4af
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:29:35 +0530

    Clean up unused variables in max product subarray
    Removed unused variables for minimum product and answer tracking.

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -6,8 +6,3 @@ class Solution {
 
         // Initialize tracking variables with the first element
         int maxProd = nums[0];
-        int minProd = nums[0];
-        int ans = nums[0];
-
-        // Iterate through the array starting from the second element
-        fo
