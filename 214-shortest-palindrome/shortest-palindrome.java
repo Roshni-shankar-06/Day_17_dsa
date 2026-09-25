93ddafe78e56f65e9c10c9a3f85f5723fca7e4e0
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-25 10:25:10 +0530

    Add shortestPalindrome method to Solution class
    Implemented a method to find the shortest palindrome by appending characters to the original string.

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,1 +1,27 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        
+        String rev = new StringBuilder(s).reverse().toString();
+        String temp = s + "#" + rev;
+        int[] lps = new int[temp.length()];
+        
+        for (int i = 1; i < temp.length(); i++) {
+            int j = lps[i - 1];
+            while (j > 0 && temp.charAt(i) != temp.charAt(j)) {
+                j = lps[j - 1];
+            }
+            if (temp.charAt(i) == temp.charAt(j)) {
+                j++;
+            }
+            lps[i] = j;
+        }
+        
+        int longestPrefixLen = lps[temp.length() - 1];
+        String rem = s.substring(longestPrefixLen);
+        return new StringBuilder(rem).reverse().toString() + s;
+    }
+}
 
